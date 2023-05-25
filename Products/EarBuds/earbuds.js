67891f8429229c53f66898041a1d5b8a55672fbc
Yagpath <vikasm027@gmail.com>
2023-05-25 19:22:52 +0530

    almost completed product description page and changed key name + removed addition bug from cart

--- Products/EarBuds/earbuds.js
+++ Products/EarBuds/earbuds.js
@@ -1,80 +1,88 @@
 var earbuds = [
     {
+        id: 1,
         image: "Images/boAt Airdopes 413 ANC.webp",
         alt: "boAt Airdopes 413 ANC",
         name: "Airdopes 413 ANC",
         rating: "5",
-        price: "₹1,999",
+        price: 1999,
         strikedoffprice: "₹4,990.00",
         discount: "60% off",
         functions: "Product functions..."
     },
     {
+        id: 2,
         image: "Images/boAt Airdopes Atom 83.webp",
         alt: "boAt Airdopes Atom 83",
         name: "Airdopes Atom 83",
         rating: "Be first to review ",
-        price: "₹1,499",
+        price: 1499,
         strikedoffprice: "₹3,490.00",
         discount: "57% off",
         functions: "Product functions..."
     },
     {
+        id: 3,
         image: "Images/boAt-Airdopes-121-PRO.webp",
         alt: "boAt-Airdopes-121-PRO",
         name: "Airdopes 121 PRO",
         rating: "4.9",
-        price: "₹1,499",
+        price: 1499,
         strikedoffprice: "₹2,990.00",
         discount: "49% off",
         functions: "Product functions..."
     },
     {
+        id: 4,
         image: "Images/boAt-Airdopes-501-ANC.webp",
         alt: "boAt-Airdopes-501-ANC",
         name: "Airdopes 501 ANC",
         rating: "5",
-        price: "₹4,990",
+        price: 4990,
         strikedoffprice: "₹9,990.00",
         discount: "80% off",
         functions: "Product functions..."
     },
     {
+        id: 6,
         image: "Images/boAt Airdopes 141.webp",
         alt: "boAt Airdopes 141",
         name: "Airdopes 141",
         rating: "4.8",
-        price: "1,399",
+        price: 1399,
         strikedoffprice: "₹4,990.00",
         discount: "68% off",
         functions: "Product functions..."
     },
     {
+        id: 7,
         image: "Images/Airdopes-393-ANC.webp",
         alt: "Airdopes-393-ANC",
         name: "Airdopes 393 ANC",
         rating: "5",
-        price: "₹2,999",
+        price: 2999,
         strikedoffprice: "₹4,990.00",
         discount: "39% off",
         functions: "Product functions..."
     },
     {
+        id: 8,
         image: "Images/boAt Airdopes 161.webp",
         alt: "boAt Airdopes 161",
         name: "Airdopes 161",
         rating: "4.9",
-        price: "₹1,099",
+        price: 1099,
         strikedoffprice: "₹2,490.00",
         discount: "55% off",
         functions: "Product functions..."
     },
     {
+        id: 9,
         image: "Images/boAt-Airdopes-192.webp",
         alt: "boAt-Airdopes-192",
         name: "Airdopes 192",
         rating: "4.8",
-        price: "₹1,999",
+        price: 1999,
         strikedoffprice: "₹3,490.00",
         discount: "42% off",
         functions: "Product functions..."
@@ -84,48 +92,52 @@ var earbuds = [
         alt: "Airdopes 411 ANC",
         name: "Airdopes 411 ANC",
         rating: "4.8",
-        price: "₹2,499",
+        price: 2499,
         strikedoffprice: "₹4,990.00",
         discount: "49% off",
         functions: "Product functions..."
     },
     {
+        id: 10,
         image: "Images/Airdopes_115.webp",
         alt: "Airdopes_115",
         name: "Airdopes 115",
         rating: "4.9",
-        price: "₹1,499",
+        price: 1499,
         strikedoffprice: "₹2,990.00",
         discount: "49% off",
         functions: "Product functions..."
     },
     {
+        id: 11,
         image: "Images/Airdopes_100.webp",
         alt: "Airdopes_100",
         name: "Airdopes 100",
         rating: "4.7",
-        price: "₹1,199",
+        price: 1199,
         strikedoffprice: "₹2,490.00",
         discount: "60% off",
         functions: "Product functions..."
     },
 
     {
+        id: 12,
         image: "Images/Airdopes-163.webp",
         alt: "Airdopes-163",
         name: "Airdopes 163",
         rating: "4.8",
-        price: "₹1,399",
+        price: 1399,
         strikedoffprice: "₹2,490.00",
         discount: "43% off",
         functions: "Product functions..."
     },
     {
+        id: 14,
         image: "Images/Airdopes-191G.webp",
         alt: "Airdopes-191G",
         name: "Airdopes 191G",
         rating: "4.9",
-        price: "₹1,799",
+        price: 1799,
         strikedoffprice: "₹3,490.00",
         discount: "48% off",
         functions: "Product functions..."
@@ -149,6 +161,10 @@ function displayearBuds() {
         image.src = element.image;
         image.alt = element.alt;
 
+        image.addEventListener("click", function () {
+            openProductPage(element);
+        });
+
         imagebox.appendChild(image);
 
         var productdatabox = document.createElement("div");
@@ -166,7 +182,7 @@ function displayearBuds() {
         priceBox.className = "pricebox";
         var price = document.createElement("p");
         price.className = "price"
-        price.textContent = element.price;
+        price.textContent = "₹" + element.price;
 
         var strikedoffprice = document.createElement("p");
         strikedoffprice.className = "strikedoffprice"
@@ -207,27 +223,32 @@ function displayearBuds() {
     });
 
     function addToCart(product) {
-        var cartProducts = JSON.parse(localStorage.getItem("cartList")) || [];
+        var cartProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
         var existingProduct = cartProducts.find(function (p) {
             return p.name === product.name;
         });
 
         if (existingProduct) {
             existingProduct.quantity += 1;
+            existingProduct.price += product.price * product.quantity; 
         } else {
             var newProduct = {
                 image: product.image_url,
                 name: product.name,
-                price: product.price,
+                price: (product.price),
                 quantity: 1
             };
             cartProducts.push(newProduct);
         }
 
-        localStorage.setItem("cartList", JSON.stringify(cartProducts));
-        
+        localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+
     }
 
+}
+
+function openProductPage(element) {
+    window.location.href = "product" + element.id + ".html";
 }
 
 
