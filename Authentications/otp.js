12b43372f614adc948280de9817d8d20a8739341
Anwar-3108 <syed.sahab9628@gmail.com>
2023-05-26 17:12:47 +0530

    modified Otp page , added timer

--- Authentications/otp.js
+++ Authentications/otp.js
@@ -1,104 +1,46 @@
 
-    // var otpTimer;
-// var timeLeft = 10; // Time limit in seconds
+    var otpTimer;
+var timeLeft = 10; // Time limit in seconds
 
-// document.getElementById("send-otp-btn").addEventListener("click", function() {
-//     event.preventDefault() ;
-//   var enteredOTP = document.getElementById("inpNum").value;
-//   var generatedOTP = 123; // Replace with the actual OTP sent to the user
+document.getElementById("send-otp-btn").addEventListener("click", function() {
+    event.preventDefault() ;
+  var enteredOTP = document.getElementById("inpNum").value;
+  var generatedOTP = "123"; // Replace with the actual OTP sent to the user
 
-//   if (enteredOTP === generatedOTP) {
-//     // stopTimer();
-//     window.location.href="home.html"
-//   } else {
-//     // document.getElementById("errormsg").textContent = "Invalid OTP. ";
-//     console.log("invalid")
-//   }
-// });
+  if (enteredOTP === generatedOTP) {
+    stopTimer();
+    window.location.href="waiting.html"
+  } else {
+    document.getElementsByClassName("errormsg")[0].textContent = "Invalid OTP. ";
+    // console.log("invalid")
+  }
+});
 
-// function startTimer() {
-//   otpTimer = setInterval(function() {
-//     if (timeLeft > 0) {
-//       document.getElementById("left").textContent = "Resend OTP in  " + timeLeft + "s";
-//       timeLeft--;
-//     } else {
-//       stopTimer();
-//       document.getElementById("errormsg").textContent = "OTP expired";
-//       document.getElementById("right").textContent = "Resend";
-//     }
-//   }, 1000);
-// }
-
-// function stopTimer() {
-//   clearInterval(otpTimer);
-// }
-
-// startTimer();
-
-
-
-/////////--t-1-------//
-
-//   var otpTimer;
-// var timeLeft = 10; // Time limit in seconds
-
-
-
-document.querySelector("form").addEventListener("submit" , getData) ;
-
-function getData(){
-  event.preventDefault() ;
-
-
-  //   var enteredOTP = document.getElementById("inpNum").value;
-  // var generatedOTP = "123"; // Replace with the actual OTP sent to the user
-
-
-  //   if (enteredOTP === generatedOTP) {
-  //   stopTimer();
-  //   window.location.href="home.html"
-  // } else {
-  //   // document.getElementById("errormsg").textContent = "Invalid OTP. ";
-  //   console.log("invalid")
-  // }
-
-let otp =  document.getElementById("inpNum").value ;
-
-if(otp ==="123"){
-  
-  window.location.href="waiting.html";
-
-}else{
-  // window.location.href="otp.html" ;
-   document.querySelector("askForCode").textContent =`Invalid OTP`;
-  document.getElementById("inpNum").textContent="" ;
+function startTimer() {
+  otpTimer = setInterval(function() {
+    if (timeLeft > 0) { 
+      // console.log(timeLeft)
+      document.getElementsByClassName("left")[0].textContent = "Resend OTP in  " + timeLeft + "s";
+      timeLeft--;
+    } else {
+      stopTimer(); 
+      document.getElementsByClassName("left")[0].textContent = "Didn't recive otp? " 
+      // document.getElementsByClassName("errormsg")[0].textContent = "OTP expired";
+      document.getElementsByClassName("right")[0].textContent = "Resend";
+    }
+  }, 1000);
 }
-
-}
-
-//     !! throwing errror while calling the function
-
-
-
-// function startTimer() {
-//   otpTimer = setInterval(function() {
-//     if (timeLeft > 0) {
-//       document.querySelector("#left").textContent =` Resend OTP in  + ${timeLeft} + s`;
-//       timeLeft--;
-//     } else {
-//       stopTimer();
-//       document.getElementById("errormsg").textContent = "OTP expired";
-//       document.getElementById("right").textContent = "Resend";
-//     }
-//   }, 1000);
-// }
-
 
 function stopTimer() {
   clearInterval(otpTimer);
 }
 
 startTimer();
+
+
+function resendOtp(){
+  
+}
 
 
 
